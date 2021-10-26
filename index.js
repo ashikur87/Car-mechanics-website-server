e9bb28cdb703bf390a3fdeddf7be5cfc480682c6
ashikur87 <ashikurrahmanswe@gmail.com>
2021-10-27 02:51:06 +0600

    app add

--- index.js
+++ index.js
@@ -83,6 +83,10 @@ app.get('/',(req,res)=>{
     
     res.send('running genius server')
 });
+app.get('/hello',(req,res)=>{
+    
+    res.send('hello world!!')
+});
 app.listen(port,()=>{
     console.log('response done',port);
 })
